8137c9033e96744181ff76c7495f8d95cc4e7ee8
Hadeer Ibraheem <hadeeribraheemothman@gmail.com>
2024-12-06 00:10:58 +0200

    Section 4 WordPress Specific PHP

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/footer.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -1,1 +1,69 @@
-<h1> Greeting from footer.php</h1>
+<footer class="site-footer">
+     <div class="site-footer__inner container container--narrow">
+          <div class="group">
+               <div class="site-footer__col-one">
+                    <h1 class="school-logo-text school-logo-text--alt-color">
+                         <a href="#"><strong>Fictional</strong> University</a>
+                    </h1>
+                    <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+               </div>
+
+               <div class="site-footer__col-two-three-group">
+                    <div class="site-footer__col-two">
+                         <h3 class="headline headline--small">Explore</h3>
+                         <nav class="nav-list">
+                              <ul>
+                              <li><a href="#">About Us</a></li>
+                              <li><a href="#">Programs</a></li>
+                              <li><a href="#">Events</a></li>
+                              <li><a href="#">Campuses</a></li>
+                              </ul>
+                         </nav>
+                    </div>
+
+                    <div class="site-footer__col-three">
+                         <h3 class="headline headline--small">Learn</h3>
+                         <nav class="nav-list">
+                              <ul>
+                              <li><a href="#">Legal</a></li>
+                              <li><a href="#">Privacy</a></li>
+                              <li><a href="#">Careers</a></li>
+                              </ul>
+                         </nav>
+                    </div>
+               </div>
+
+               <div class="site-footer__col-four">
+                    <h3 class="headline headline--small">Connect With Us</h3>
+                    <nav>
+                         <ul class="min-list social-icons-list group">
+                              <li>
+                                   <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                              </li>
+                              <li>
+                                   <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                              </li>
+                              <li>
+                                   <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                              </li>
+                              <li>
+                                   <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                              </li>
+                              <li>
+                                   <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                              </li>
+                         </ul>
+                    </nav>
+               </div>
+          </div>
+     </div>
+</footer>
+<?php wp_footer(); ?>
+     <!-- wp_footer() Function in WordPress
+     The wp_footer() function is a built-in WordPress hook used to add content, scripts, or functionality to the footer section of a WordPress theme. It is one of the most important hooks and is typically used by plugins and themes to insert code before the closing <body> tag.
+
+     Purpose
+     It outputs crucial JavaScript, styles, and other content added by WordPress core, themes, or plugins.
+     Ensures proper functionality of features like analytics tracking, JavaScript libraries, or other third-party scripts. -->
+</body>
+</html>
